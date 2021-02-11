e84e19b6a051d2966d1bdabbb7b6ab4984c8e51e
Georgios <georgios.salonikidis@adesso.de>
2021-02-11 21:54:13 +0100

    event bindings

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 import { Book } from '../shared/book';
 
@@ -9,6 +9,7 @@ import { Book } from '../shared/book';
 })
 export class BookListComponent implements OnInit {
   books: Book[];
+  @Output() showDetailsEvent = new EventEmitter<Book>();
 
   ngOnInit(): void {
     this.books = [
@@ -39,5 +40,9 @@ export class BookListComponent implements OnInit {
         description: 'Das bewährte und umfassende Praxisbuch zu React.'
       }
     ];
+  }
+
+  showDetails(book: Book) {
+    this.showDetailsEvent.emit(book);
   }
 }
